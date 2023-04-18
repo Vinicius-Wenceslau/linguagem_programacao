7fe6fa9a1b6ac3479aaa64ba49164aa755508655
Vinicius-Wenceslau <130321264+Vinicius-Wenceslau@users.noreply.github.com>
2023-04-18 07:58:39 -0300

    Add files via upload

---
+++ aluno.py
@@ -0,0 +1,2 @@
+from classe import  aluno
+vinicius = aluno(1, 'Vinicius', 2.0, 2.0, 1.5)
---
+++ classe.py
@@ -0,0 +1,9 @@
+class aluno:
+    def __init__(self, matricula, nome, prova1, prova2, trabalho1):
+        self.matricula = matricula
+        self.nome = nome
+        self.prova1 = prova1
+        self.prova2 = prova2
+        self.trabalho1 = trabalho1
+    
+
